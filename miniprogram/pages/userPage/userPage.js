466f0fd9da8390dd3702251fa2e6987d52facde1
youngtree <847398959@qq.com>
2019-01-13 16:05:19 +0800

    revert

--- miniprogram/pages/userPage/userPage.js
+++ miniprogram/pages/userPage/userPage.js
@@ -2,31 +2,9 @@
  * @author wjy
  * @description 我的信息
  */
-const app = getApp()
+
 Page({
   data: {
-    canIUse: false,
-    user: ''
-  },
-  onShow: function () {
-    this.user = app.globalData.userInfo
-  },
-  onLoad: function () {
-    // 查看是否授权
-    wx.getSetting({
-      success(res) {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function (res) {
-              console.log(res)
-            }
-          })
-        }
-      }
-    })
-  },
-  bindGetUserInfo(e) {
-    console.log(e.detail.userInfo)
+    userPicUrl: "../../images/icons/user_default.png"
   }
 });
